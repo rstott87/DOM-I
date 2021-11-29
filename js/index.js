947d8fbb77294a051b61dcea10668f6a7dae8376
rstott87 <rstott87@yahoo.com>
2021-11-29 11:06:24 -0500

    finished project

--- js/index.js
+++ js/index.js
@@ -55,5 +55,61 @@ button.innerHTML = siteContent["cta"]["button"];
 let ctaImg = document.getElementById("cta-img");
 ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
 
+// ** MAIN CONTENT SECTION **
+//top
+let featuresH4 = document.querySelector(".top-content div:nth-child(1) h4");
+featuresH4.innerHTML = siteContent["main-content"]["features-h4"];
 
+let featuresPar = document.querySelector(".top-content div:nth-child(1) p");
+featuresPar.innerHTML = siteContent["main-content"]["features-content"];
+
+let aboutH4 = document.querySelector(".top-content div:nth-child(2) h4");
+aboutH4.innerHTML = siteContent["main-content"]["about-h4"];
+
+let aboutPar = document.querySelector(".top-content div:nth-child(2) p");
+aboutPar.innerHTML = siteContent["main-content"]["about-content"];
+
+//middle 
+
+let middleImg = document.querySelector("#middle-img")
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+//bottom
+
+let servicesH4 = document.querySelector(".bottom-content div:nth-child(1) h4");
+servicesH4.innerHTML = siteContent["main-content"]["services-h4"];
+
+let servicesPar = document.querySelector(".bottom-content div:nth-child(1) p");
+servicesPar.innerHTML = siteContent["main-content"]["services-content"];
+
+let productH4 = document.querySelector(".bottom-content div:nth-child(2) h4");
+productH4.innerHTML = siteContent["main-content"]["product-h4"];
+
+let productPar = document.querySelector(".bottom-content div:nth-child(2) p");
+productPar.innerHTML = siteContent["main-content"]["product-content"];
+
+let visionH4 = document.querySelector(".bottom-content div:nth-child(3) h4");
+visionH4.innerHTML = siteContent["main-content"]["vision-h4"];
+
+let visionPar = document.querySelector(".bottom-content div:nth-child(3) p");
+visionPar.innerHTML = siteContent["main-content"]["vision-content"];
+
+// ** CONTACT **
+
+let contactH4 =  document.querySelector(".contact h4");
+contactH4.innerHTML = siteContent["contact"]["contact-h4"]
+
+let address = document.querySelector(".contact p:nth-child(2)");
+address.innerHTML = siteContent["contact"]["address"]
+
+let phone = document.querySelector(".contact p:nth-child(3)")
+phone.innerHTML = siteContent["contact"]["phone"]
+
+let email = document.querySelector(".contact p:nth-child(4)")
+email.innerHTML = siteContent["contact"]["email"]
+
+// ** FOOTER **
+
+let footer = document.querySelector("footer p");
+footer.innerHTML = siteContent["footer"]["copyright"];
 
